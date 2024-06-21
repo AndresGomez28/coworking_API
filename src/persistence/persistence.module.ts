35e0167e3a4b87299dd1d8b1568a2557d2a43121
Andres Gomez <pipe2893@gmail.com>
2024-06-21 06:43:55 -0500

    DB conection creted

---
+++ src/persistence/persistence.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeOrmConfig } from './db-config';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: typeOrmConfig,
+        }),
+        TypeOrmModule.forFeature([]),
+    ],
+    exports: [TypeOrmModule],
+})
+export class PersistenceModule {}
